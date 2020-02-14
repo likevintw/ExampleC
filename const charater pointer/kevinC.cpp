7e980d2e9be0dc54a2bf8690d76796a47e38374f
kevin <likevintw@gail.com>
2020-02-14 15:43:45 +0800

    Updated

---
+++ const charater pointer/kevinC.cpp
@@ -0,0 +1,25 @@
+﻿// const char
+#include<stdio.h>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+	char A[10] = "Hello";
+	char B[10] = "World";
+	int len = 20;
+	char C[20];
+	const char *D;
+	memset(C, '\0', len);
+	strcat(C, A);
+	D = A;
+	printf("%s \n%s\n", A, D);
+	strcat(C, " ");
+	strcat(C, B);
+	D = B;
+	printf("%s \n%s\n", B, D);
+	D = C;
+	printf("%s \n%s\n", C,D );
+	return 0;
+}
